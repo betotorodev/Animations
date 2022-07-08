e95baaf8637c3529322e02ebfb3f2d3b991301da
Beto Toro <beto.toro@zemoga.com>
2022-07-08 15:22:36 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Beto Toro on 8/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
